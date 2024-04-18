72a37afa76f7e7101c1907b792fca9ba5af0f0fe
zavvdev <zaviryukha.igor99@gmail.com>
2024-04-18 22:34:05 +0300

    added sub/mul/div function parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-pub mod types;
-pub mod messages;
 pub mod lexer;
+pub mod messages;
 pub mod parser;
+pub mod types;
 
 use lexer::tokenize;
 use parser::parse;
 
 fn main() {
-    let tokens = tokenize("@add(2, 2)");
+    let tokens = tokenize(
+        "
+        @add(
+            @sub(4 2)
+            @mul(2 2)
+            @div(4 2))        
+    ",
+    );
     let parser_ast = parse(tokens);
 
     println!("{:#?}", parser_ast);
