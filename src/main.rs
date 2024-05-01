ac7ba71d25c33189e12bfc70367c148b8f9457b0
zavvdev <zaviryukha.igor99@gmail.com>
2024-05-01 23:29:30 +0300

    added sample.txt file

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,21 @@ pub mod lexer;
 pub mod parser;
 pub mod types;
 
+use std::fs;
+
 use interpreter::interpret;
 use lexer::tokenize;
 use parser::parse;
 
 fn main() {
-    let tokens = tokenize(
-        "
-        @println(123) 
-    ",
-    );
-
-    let parser_ast = parse(tokens);
-
-    interpret(&parser_ast);
+    match fs::read_to_string("sample.txt") {
+        Ok(content) => {
+            let tokens = tokenize(&content);
+            let parser_ast = parse(tokens);
+            interpret(&parser_ast);
+        }
+        Err(e) => {
+            println!("Cannot read sample.txt: {}", e);
+        }
+    }
 }
