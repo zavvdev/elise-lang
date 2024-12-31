66a5bcd336dbeda6cc5c862d7c50ea9216494372
zavvdev <zaviryukha.igor99@gmail.com>
2025-01-01 00:08:59 +0200

    ref-int: updated tests for and fn

--- src/interpreter/__tests__/test_and.rs
+++ src/interpreter/__tests__/test_and.rs
@@ -2,63 +2,82 @@
 mod tests {
     use crate::{
         interpreter::{
-            eval,
+            interpret,
             models::env::{Env, EvalResult},
         },
         parser::models::expression::{Expr, ExprKind},
     };
 
+    // SUCCESS CASES
+
     #[test]
-    fn test_and() {
+    fn test_empty() {
         let mut env = Env::new();
 
         assert_eq!(
-            eval(&Expr::new(ExprKind::FnAnd, vec![], 0), &mut env),
-            EvalResult::Boolean(true)
+            interpret(
+                &vec![Expr::new(ExprKind::FnAnd, vec![], 0)],
+                &mut env,
+                ".and()"
+            ),
+            vec![EvalResult::Boolean(true)]
         );
+    }
+
+    #[test]
+    fn test_number() {
+        let mut env = Env::new();
 
         assert_eq!(
-            eval(
-                &Expr::new(
+            interpret(
+                &vec![Expr::new(
                     ExprKind::FnAnd,
                     vec![Box::new(Expr::new(ExprKind::Number(2.2), vec![], 0))],
                     0
-                ),
-                &mut env
+                )],
+                &mut env,
+                ".and(2.2)"
             ),
-            EvalResult::Number(2.2)
+            vec![EvalResult::Number(2.2)]
         );
+    }
+
+    #[test]
+    fn test_false() {
+        let mut env = Env::new();
 
         assert_eq!(
-            eval(
-                &Expr::new(
+            interpret(
+                &vec![Expr::new(
                     ExprKind::FnAnd,
                     vec![Box::new(Expr::new(ExprKind::Boolean(false), vec![], 0))],
                     0
-                ),
-                &mut env
+                )],
+                &mut env,
+                ".and(false)"
             ),
-            EvalResult::Boolean(false)
+            vec![EvalResult::Boolean(false)]
         );
 
         assert_eq!(
-            eval(
-                &Expr::new(
+            interpret(
+                &vec![Expr::new(
                     ExprKind::FnAnd,
                     vec![
                         Box::new(Expr::new(ExprKind::Boolean(false), vec![], 0)),
                         Box::new(Expr::new(ExprKind::Nil, vec![], 0))
                     ],
                     0
-                ),
-                &mut env
+                )],
+                &mut env,
+                ".and(false, nil)"
             ),
-            EvalResult::Boolean(false)
+            vec![EvalResult::Boolean(false)]
         );
 
         assert_eq!(
-            eval(
-                &Expr::new(
+            interpret(
+                &vec![Expr::new(
                     ExprKind::FnAnd,
                     vec![
                         Box::new(Expr::new(ExprKind::Boolean(false), vec![], 0)),
@@ -66,25 +85,32 @@ mod tests {
                         Box::new(Expr::new(ExprKind::String("123".to_string()), vec![], 0))
                     ],
                     0
-                ),
-                &mut env
+                )],
+                &mut env,
+                ".and(false, nil, \"123\")"
             ),
-            EvalResult::Boolean(false)
+            vec![EvalResult::Boolean(false)]
         );
+    }
+
+    #[test]
+    fn test_last() {
+        let mut env = Env::new();
 
         assert_eq!(
-            eval(
-                &Expr::new(
+            interpret(
+                &vec![Expr::new(
                     ExprKind::FnAnd,
                     vec![
                         Box::new(Expr::new(ExprKind::String("123".to_string()), vec![], 0)),
                         Box::new(Expr::new(ExprKind::Nil, vec![], 0)),
                     ],
                     0
-                ),
-                &mut env
+                )],
+                &mut env,
+                ".and(\"123\", nil)"
             ),
-            EvalResult::Nil
+            vec![EvalResult::Nil]
         );
     }
 }
