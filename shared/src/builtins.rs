57f495aa774572e2c6dba93b556a8e54fbfe4d16
zavvdev <zaviryukha.igor99@gmail.com>
2026-04-04 20:14:45 +0100

    removed builtins

---
+++ shared/src/builtins.rs
@@ -0,0 +1,6 @@
+pub const FN_DEFINE: &str = "define";
+pub const FN_LET: &str = "let";
+pub const FN_ADD: &str = "add";
+pub const FN_SUB: &str = "sub";
+pub const FN_MUL: &str = "mul";
+pub const FN_DIV: &str = "div";
