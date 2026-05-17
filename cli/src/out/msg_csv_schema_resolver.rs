c156a642499cd6f1786b73ba0d70a0c07479213b
zavvdev <zaviryukha.igor99@gmail.com>
2026-05-17 12:21:28 +0100

    formatting

--- cli/src/out/msg_csv_schema_resolver.rs
+++ cli/src/out/msg_csv_schema_resolver.rs
@@ -9,9 +9,13 @@ pub fn print_err(schema_err: &CsvSchemaResolverErr) {
         EmptySchema => "Schema definition file cannot be empty".to_string(),
         InvalDef { pos: _ } => "Invalid schema definition function".to_string(),
         InvalRowDef { pos: _ } => "Invalid row definition".to_string(),
-        TooManySchemaDefArgs { pos: _ } => "Schema definition function should have only one argument".to_string(),
+        TooManySchemaDefArgs { pos: _ } => {
+            "Schema definition function should have only one argument".to_string()
+        }
         EmptyRow { pos: _ } => "Row definition does not have any column descriptors".to_string(),
-        RowInvalArgsLen { pos: _ } => "Arguments length for the row definition function must be even".to_string(),
+        RowInvalArgsLen { pos: _ } => {
+            "Arguments length for the row definition function must be even".to_string()
+        }
         InvalColName { pos: _ } => "Invalid column name".to_string(),
         InvalColTypeDef { pos: _ } => "Invalid column type definition".to_string(),
         Unknown => "Unexpected error".to_string(),
