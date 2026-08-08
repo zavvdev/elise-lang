c7c595ab5d5fef876b59088cbe81030f746b1c31
zavvdev <zaviryukha.igor99@gmail.com>
2026-08-08 15:02:32 +0100

    (broken): Added comment

--- frontend/data/src/schema_resolver.rs
+++ frontend/data/src/schema_resolver.rs
@@ -228,6 +228,7 @@ impl<'a> SchemaResolver<'a> {
             current_type: None,
             // Track current modifier in order to be able to provide metadata
             // for the type being resolved.
+            // TODO: Replace with LinkedList.
             current_modifiers: vec![],
         }
     }
